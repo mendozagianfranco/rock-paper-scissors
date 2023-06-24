74a33bd2595522497c6fe71a8ae443db38eebfde
Gianfranco Mendoza <gianfranco.mendoza123@gmail.com>
2023-06-24 16:20:35 +0200

    Update game
    Placed graphical interface with style.css
    Adequate javascript logic

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ let scoreUser = 0;
 let scoreComputer = 0;
 
 let buttons = document.querySelectorAll('button');
-let container = document.querySelector('.container');
+let result = document.querySelector('.text-result');
+let player = document.querySelector('.score__player');
+let computer = document.querySelector('.score__computer');
 
 buttons.forEach((btn) => {
     btn.addEventListener('click', function game(e) {
         const play = playRound(e.target.textContent, getComputerChoice());
-        let textResult = play.textContent;
 
-        if (textResult.includes('You win')) {
+        if (play.includes('You win')) {
             scoreUser++;
-        } else if (textResult.includes('You lose')) {
+            player.textContent = `${scoreUser}`;
+        } else if (play.includes('You lose')) {
             scoreComputer++;
+            computer.textContent = `${scoreComputer}`;
         }
 
-        let winner = document.createElement('div');
         if (scoreUser === 5) {
-            winner.textContent = `You win! Player score: ${scoreUser} Computer score: ${scoreComputer}`;
-            container.appendChild(winner);
+            result.textContent = `You win! Player score: ${scoreUser} Computer score: ${scoreComputer}`;
             btn.removeEventListener('click', game);
         } else if (scoreComputer === 5) {
-            winner.textContent = `You lose! Player score: ${scoreUser} Computer score: ${scoreComputer}`;
-            container.appendChild(winner);
+            result.textContent = `You lose! Player score: ${scoreUser} Computer score: ${scoreComputer}`;
             btn.removeEventListener('click', game);
         }
     });
@@ -54,51 +54,16 @@ function playRound(playerSelection, computerSelection) {
     // I converted  the choice of player and computer into capital letters to compare them better.
     let player = playerSelection.toUpperCase();
     let computer = computerSelection.toUpperCase();
-    let result = document.createElement('div');
 
     if (player === computer) {
-        result.textContent = `Draw! Player: "${playerSelection}" and Computer: "${computerSelection}"`;
-        return container.appendChild(result);
+        return (result.textContent = `Draw! Player: "${playerSelection}" and Computer: "${computerSelection}"`);
     } else if (
         (player === 'PAPER' && computer === 'SCISSORS') ||
         (player === 'SCISSORS' && computer === 'ROCK') ||
         (player === 'ROCK' && computer === 'PAPER')
     ) {
-        result.textContent = `You lose! Computer: "${computerSelection}" beats Player: "${playerSelection}"`;
-        return container.appendChild(result);
+        return (result.textContent = `You lose! Computer: "${computerSelection}" beats Player: "${playerSelection}"`);
     } else {
-        result.textContent = `You win! Player: "${playerSelection}" beats Computer: "${computerSelection}"`;
-        return container.appendChild(result);
-    }
-}
-
-/* Games 5 rounds with final score */
-
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        let userChoice = prompt("Let's play rock paper scissors", '');
-        let play = playRound(userChoice, getComputerChoice());
-
-        console.log(play);
-
-        if (play.includes('You win')) {
-            scoreUser++;
-        } else if (play.includes('You lose')) {
-            scoreComputer++;
-        }
-    }
-
-    if (scoreUser > scoreComputer) {
-        console.log(
-            `You win! score User:${scoreUser} score Computer:${scoreComputer} `
-        );
-    } else if (scoreUser < scoreComputer) {
-        console.log(
-            `You lose! score User:${scoreUser} score Computer:${scoreComputer} `
-        );
-    } else {
-        console.log(
-            `Draw! score User:${scoreUser} score Computer:${scoreComputer} `
-        );
+        return (result.textContent = `You win! Player: "${playerSelection}" beats Computer: "${computerSelection}"`);
     }
 }
